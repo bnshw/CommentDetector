cb0cd5d071f0ad2719e273a3efc7450886821b9b
benASTRO <81444451+benASTRO@users.noreply.github.com>
2021-11-18 22:05:16 +0100

    Update build.ps1

--- CommentDetector/build.ps1
+++ CommentDetector/build.ps1
@@ -1,3 +1,3 @@
 ﻿dotnet publish -o ..\Build -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true .\CommentDetector.csproj
 xcopy .\Json\settings.json ..\Build /E /Y /I
-xcopy ..\Build C:\Users\BenSc\Documents\ApuDoingStuff\CommentDetector /E /Y /I
+xcopy ..\Build {Path}\CommentDetector /E /Y /I
